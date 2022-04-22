44be0d7f8857eb8b31a372a29713180972ad874d
Omar Badreldin <omarbdreldin@gmail.com>
2022-04-22 13:10:09 +0200

    Creating NavHostActivity.kt for use in activities that host NavHostFragment

---
+++ base/src/main/java/com/omarbadreldin/base/activity/NavHostActivity.kt
@@ -0,0 +1,28 @@
+package com.omarbadreldin.base.activity
+
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.viewbinding.ViewBinding
+import com.omarbadreldin.base.constants.ErrorMessages.Dev.MESSAGE_NAV_HOST_FRAGMENT_NOT_FOUND
+
+abstract class NavHostActivity<BINDING : ViewBinding> : BaseActivity<BINDING>() {
+
+    private lateinit var navHostFragment: NavHostFragment
+
+    protected abstract val navHostFragmentId: Int
+        @IdRes get
+
+    protected val navController: NavController
+        get() = navHostFragment.navController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navHostFragment = supportFragmentManager.findFragmentById(
+            navHostFragmentId
+        ) as? NavHostFragment ?: throw RuntimeException(
+            message = MESSAGE_NAV_HOST_FRAGMENT_NOT_FOUND
+        )
+    }
+}
---
+++ base/src/main/java/com/omarbadreldin/base/constants/ErrorMessages.kt
@@ -0,0 +1,10 @@
+package com.omarbadreldin.base.constants
+
+object ErrorMessages {
+
+    object Dev {
+        val MESSAGE_NAV_HOST_FRAGMENT_NOT_FOUND = """Couldn't initialize navHostFragment.
+            |Invalid id or fragment type.
+        """.trimMargin()
+    }
+}
