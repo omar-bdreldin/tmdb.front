eeab157a8d602d4bd1e573390c3e4252fade0496
Omar Badreldin <omarbdreldin@gmail.com>
2022-04-22 23:44:37 +0200

    Preliminary integration of MoviesListingFragment.kt with paging bases

---
+++ app/src/main/java/com/omarbadreldin/teldamoviestask/ui/listing/MovieListingMVI.kt
@@ -0,0 +1,27 @@
+package com.omarbadreldin.teldamoviestask.ui.listing
+
+import com.omarbadreldin.base.data.paging.*
+import com.omarbadreldin.base.mvi.MVI
+
+interface MovieListingMVI : MVI {
+
+    interface View : PagingView<Intent, State, Model, Page>
+
+    interface Model : PagingModel<Intent, State>
+
+    sealed interface Intent : MVI.Intent {
+        object LoadPage : Intent, com.omarbadreldin.base.data.paging.LoadPage
+    }
+
+    sealed interface State : MVI.State {
+
+        data class ListLoading(
+            override val isLoading: Boolean
+        ) : State, com.omarbadreldin.base.data.paging.ListLoading
+
+        data class PageLoaded(
+            override val page: Page
+        ) : State, com.omarbadreldin.base.data.paging.PageLoaded<Page>
+
+    }
+}
--- app/src/main/java/com/omarbadreldin/teldamoviestask/ui/listing/MoviesListingFragment.kt
+++ app/src/main/java/com/omarbadreldin/teldamoviestask/ui/listing/MoviesListingFragment.kt
@@ -1,11 +1,51 @@
 package com.omarbadreldin.teldamoviestask.ui.listing
 
+import android.os.Bundle
+import android.view.View
+import com.omarbadreldin.base.data.paging.PagingAdapter
 import com.omarbadreldin.base.fragment.BaseFragment
+import com.omarbadreldin.base.recycler.BindingViewHolder
+import com.omarbadreldin.base.recycler.LayoutResSupplier
+import com.omarbadreldin.base.recycler.ListItem
+import com.omarbadreldin.base.recycler.ViewHolderCreator
+import com.omarbadreldin.teldamoviestask.R
 import com.omarbadreldin.teldamoviestask.databinding.FragmentMoviesListingBinding
 
-class MoviesListingFragment : BaseFragment<FragmentMoviesListingBinding>() {
+class MoviesListingFragment
+    : BaseFragment<FragmentMoviesListingBinding>(),
+    MovieListingMVI.View, LayoutResSupplier, ViewHolderCreator<ListItem<*>> {
 
     override val bindingInflater: () -> FragmentMoviesListingBinding = {
         FragmentMoviesListingBinding.inflate(layoutInflater)
+    }
+
+    override lateinit var adapter: PagingAdapter
+
+    override val viewModel: MovieListingMVI.Model
+        get() = TODO("Not yet implemented")
+
+    override fun onScrolledToEnd() {
+        viewModel.onIntent(MovieListingMVI.Intent.LoadPage)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = PagingAdapter(
+            layoutResSupplier = this,
+            viewHolderCreator = this,
+            onScrolledToEndListener = this,
+            loadingLayoutRes = R.layout.list_item_loading,
+        )
+    }
+
+    override fun getLayoutResForViewType(viewType: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createViewHolderForViewType(
+        viewType: Int,
+        view: View
+    ): BindingViewHolder<*, ListItem<*>> {
+        TODO("Not yet implemented")
     }
 }
