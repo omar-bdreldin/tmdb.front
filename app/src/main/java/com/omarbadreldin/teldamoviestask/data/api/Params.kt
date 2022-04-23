dd1b27db319277950aab75f0fe3ca1ac90917076
Omar Badreldin <omarbdreldin@gmail.com>
2022-04-23 10:08:10 +0200

    Adding constant wrapper for API parameters

---
+++ app/src/main/java/com/omarbadreldin/teldamoviestask/data/api/Params.kt
@@ -0,0 +1,10 @@
+package com.omarbadreldin.teldamoviestask.data.api
+
+object Params {
+
+    object Keys {
+        const val PARAM_PAGE = "page"
+        const val PARAM_PER_PAGE = "limit"
+        const val PARAM_API_KEY = "api_key"
+    }
+}
