7e0b0f07443d6ca935214c2f932a2066160a0210
Omar Badreldin <omarbdreldin@gmail.com>
2022-04-23 17:16:39 +0200

    Removing trailing / from BASE_URL

--- app/src/main/java/com/omarbadreldin/teldamoviestask/inject/NetworkModule.kt
+++ app/src/main/java/com/omarbadreldin/teldamoviestask/inject/NetworkModule.kt
@@ -10,12 +10,15 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private const val REQUEST_TIME_OUT = (1000 * 60).toLong()
 
     @Singleton
     @Provides
@@ -24,16 +27,18 @@ object NetworkModule {
             install(ContentNegotiation) {
                 json()
             }
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = REQUEST_TIME_OUT
+            }
             defaultRequest {
-                url(urlString = BuildConfig.BASE_URL)
+                url(urlString = "${BuildConfig.BASE_URL}/")
                 url {
                     parameters.append(Params.Keys.PARAM_API_KEY, BuildConfig.TMDB_API_KEY)
                 }
-            }
-        }.apply {
-            plugin(HttpSend).intercept { request ->
-                println(request.url.buildString())
-                execute(request)
             }
         }
     }
