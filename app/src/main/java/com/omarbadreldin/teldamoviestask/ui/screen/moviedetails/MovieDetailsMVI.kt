01988884a5126ced818961fa589401c39ed7e8eb
Omar Badreldin <omarbdreldin@gmail.com>
2022-04-24 17:13:24 +0200

    Adding states in MovieDetailsMVI.kt

--- app/src/main/java/com/omarbadreldin/teldamoviestask/ui/screen/moviedetails/MovieDetailsMVI.kt
+++ app/src/main/java/com/omarbadreldin/teldamoviestask/ui/screen/moviedetails/MovieDetailsMVI.kt
@@ -1,6 +1,9 @@
 package com.omarbadreldin.teldamoviestask.ui.screen.moviedetails
 
 import com.omarbadreldin.base.mvi.MVI
+import com.omarbadreldin.base.mvi.common.LoadingState
+import com.omarbadreldin.teldamoviestask.data.model.credits.Cast
+import com.omarbadreldin.teldamoviestask.data.model.movie.Movie
 import com.omarbadreldin.teldamoviestask.data.model.movie.details.MovieDetails
 
 interface MovieDetailsMVI : MVI {
@@ -14,6 +17,9 @@ interface MovieDetailsMVI : MVI {
     }
 
     sealed interface State : MVI.State {
+        data class Loading(override val isLoading: Boolean) : State, LoadingState
         data class MovieDetailsLoaded(val movieDetails: MovieDetails) : State
+        data class SimilarMoviesLoaded(val similarMovies: List<Movie>) : State
+        data class CastLoaded(val castGroupedByDepartment: Map<String, List<Cast>>) : State
     }
 }
