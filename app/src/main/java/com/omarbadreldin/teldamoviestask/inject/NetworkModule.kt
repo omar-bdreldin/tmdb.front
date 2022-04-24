aa613105d830255a91ad4dfa455c84ed65c0d501
Omar Badreldin <omarbdreldin@gmail.com>
2022-04-24 20:27:35 +0200

    Setting rules for parser

--- app/src/main/java/com/omarbadreldin/teldamoviestask/inject/NetworkModule.kt
+++ app/src/main/java/com/omarbadreldin/teldamoviestask/inject/NetworkModule.kt
@@ -12,34 +12,58 @@ import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val REQUEST_TIME_OUT = (1000 * 60).toLong()
-
     @Singleton
     @Provides
     fun provideHttpClient(): HttpClient {
         return HttpClient(CIO) {
-            install(ContentNegotiation) {
-                json()
+            installParser()
+            installLogger()
+            installTimeOut()
+            setupDefaultRequest()
+        }
+    }
+}
+
+private fun HttpClientConfig<CIOEngineConfig>.installParser() {
+    install(ContentNegotiation) {
+        json(
+            json = Json {
+                encodeDefaults = true
+                isLenient = true
+                prettyPrint = false
+                ignoreUnknownKeys = true
             }
-            install(Logging) {
-                logger = Logger.ANDROID
-                level = LogLevel.ALL
-            }
-            install(HttpTimeout) {
-                requestTimeoutMillis = REQUEST_TIME_OUT
-            }
-            defaultRequest {
-                url(urlString = "${BuildConfig.BASE_URL}/")
-                url {
-                    parameters.append(Params.Keys.PARAM_API_KEY, BuildConfig.TMDB_API_KEY)
-                }
-            }
+        )
+    }
+}
+
+private fun HttpClientConfig<CIOEngineConfig>.installLogger() {
+    install(Logging) {
+        logger = Logger.ANDROID
+        level = LogLevel.ALL
+    }
+}
+
+private const val REQUEST_TIME_OUT = (1000 * 60).toLong()
+
+private fun HttpClientConfig<CIOEngineConfig>.installTimeOut() {
+    install(HttpTimeout) {
+        requestTimeoutMillis = REQUEST_TIME_OUT
+    }
+}
+
+private fun HttpClientConfig<CIOEngineConfig>.setupDefaultRequest() {
+    defaultRequest {
+        url(urlString = "${BuildConfig.BASE_URL}/")
+        url {
+            parameters.append(Params.Keys.PARAM_API_KEY, BuildConfig.TMDB_API_KEY)
         }
     }
 }
