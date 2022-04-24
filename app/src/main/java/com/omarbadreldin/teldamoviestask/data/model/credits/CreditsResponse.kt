6db72db08a0f811e6709220bcb64898e40161a03
Omar Badreldin <omarbdreldin@gmail.com>
2022-04-24 15:45:18 +0200

    Adding models for credits API

---
+++ app/src/main/java/com/omarbadreldin/teldamoviestask/data/model/credits/CreditsResponse.kt
@@ -0,0 +1,15 @@
+package com.omarbadreldin.teldamoviestask.data.model.credits
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CreditsResponse(
+    @SerialName("cast")
+    val cast: List<Cast>? = null,
+    @SerialName("crew")
+    val crew: List<Crew>? = null,
+    @SerialName("id")
+    val id: Int? = null
+)
